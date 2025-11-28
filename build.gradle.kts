2c6cf1bf7dbc7db3487e9222b62b23a55b32ba36
CincyAndroiDeveloper <6299370+CincyAndroiDeveloper@users.noreply.github.com>
2025-11-28 17:42:07 -0500

    - Added maven-publish configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.android.lint) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
